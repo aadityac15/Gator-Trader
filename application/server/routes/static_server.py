df70d651dadf7c869a7a6a18ce218ce287cdbb8f
Sean Darryanto <seandarryanto@gmail.com>
2019-09-22 14:38:20 -0700

    Moved client to backend for serving

--- application/server/routes/static_server.py
+++ application/server/routes/static_server.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, render_template
 
-static_blueprint = Blueprint('static_server', __name__)
+static_blueprint = Blueprint('static_server',
+                             __name__,
+                             static_folder='../client',
+                             template_folder='../client/public')
 
 
-@static_blueprint.route('/about_pages/<path:name>', methods=['GET'])
-def render_about_pages(name):
-    return send_from_directory('static/about_pages', 'about_{}.html'.format(name))
+@static_blueprint.route('/', methods=['GET'])
+def render_home():
+    return render_template('index.html')
+
+@static_blueprint.route('/about', methods=['GET'])
+def render_about():
+    return render_template('about_us.html')
