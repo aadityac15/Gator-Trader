2145e73b623f0998f2635846212950f5c122ff21
Sean Darryanto <seandarryanto@gmail.com>
2019-09-22 13:50:45 -0700

    lint

--- application/server/routes/static_server.py
+++ application/server/routes/static_server.py
@@ -2,6 +2,7 @@ from flask import Blueprint, send_from_directory
 
 static_blueprint = Blueprint('static_server', __name__)
 
+
 @static_blueprint.route('/about_pages/<path:name>', methods=['GET'])
 def render_about_pages(name):
     return send_from_directory('static/about_pages', 'about_{}.html'.format(name))
