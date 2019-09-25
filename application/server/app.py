2eb2eccb564f5db935166647b0ef0f0d777649ec
davidchungdev <45413233+davidchungdev@users.noreply.github.com>
2019-09-24 22:13:31 -0700

    Milestone 0
    Home and About page
    About page for each engineer on the team
    Flask server setup/routing

---
+++ application/server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, send_from_directory
+from routes.about_me_static import about_blueprint
+from routes.static_server import static_blueprint
+
+app = Flask(__name__, static_url_path='', static_folder='client')
+
+app.register_blueprint(static_blueprint)
+app.register_blueprint(about_blueprint)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
