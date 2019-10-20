3c618c61a9cadae12571da376673c6b5e12404a6
aadityac15 <achokshi@mail.sfsu.edu>
2019-10-20 00:04:16 -0700

    Image

--- application/server/client/search.js
+++ application/server/client/search.js
@@ -1,58 +1,61 @@
 const fetchData = async () => {
-  clearData(); //Temporary
-  let dataReceived = undefined;
-  let dataLength = 0;
-  let name = "";
-  let major = "";
-  let dummyLength = 0;
-  let dummyData = undefined;
-  const table = document.querySelector("table");
-  table.style["border"] = "1px black solid";
+    clearData(); //Temporary
+    let dataReceived = undefined;
+    let dataLength = 0;
+    let dummyLength = 0;
+    let dummyData = undefined;
+    const table = document.querySelector("table");
+    table.style["border"] = "1px black solid";
 
-  console.log("in the etch data");
+    console.log("in the etch data");
 
-  await fetch("./dummy_data.json", {
-      method: 'GET',
-    })
-    .then(response => response.json())
-    .then(data => {
-      dummyData = data[0];
-      //As the data would be an object
-      dummyLength = Object.keys(dummyData).length;
-      console.log("Type of", typeof dummyData);
-      console.log(dummyData);
-      console.log("The length is", Object.keys(dummyData).length);
-    });
+    await fetch("./dummy_data.json", {
+            method: 'GET',
+        })
+        .then(response => response.json())
+        .then(data => {
+            dummyData = data[0];
+            //As the data would be an object
+            dummyLength = Object.keys(dummyData).length;
+            console.log("Type of", typeof dummyData);
+            console.log(dummyData);
+            console.log("The length is", Object.keys(dummyData).length);
+        });
 
-  for (let i = 0; i < dummyLength; i++) {
-    let tr = document.createElement("tr");
-    tr.style["border-bottom"] = "1px solid black";
-    console.log(dummyData[i]);
-    let dataCategories = Object.keys(dummyData[i]);
-    for (let j = 0; j < dataCategories.length; j++) {
-      let bTag = document.createElement("b");
-      let td1 = document.createElement("td");
-      let td2 = document.createElement("td");
+    for (let i = 0; i < dummyLength; i++) {
+        let tr = document.createElement("tr");
+        tr.style["border-bottom"] = "1px solid black";
+        console.log(dummyData[i]);
+        let dataCategories = Object.keys(dummyData[i]);
+        for (let j = 0; j < dataCategories.length; j++) {
+            let bTag = document.createElement("b");
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
 
-      td1.textContent = dataCategories[j];
-      bTag.appendChild(td1);
-      td2.textContent = dummyData[i][dataCategories[j]];
-      // console.log("td = ", td);
-      tr.appendChild(bTag);
-      tr.appendChild(td2);
+            td1.textContent = dataCategories[j];
+            if (dataCategories[j]==="image") {
+
+            let imgTag = document.createElement("img");
+            imgTag.src = dummyData[i][dataCategories[j]]
+            bTag.appendChild(td1);
+          }
+          td2.textContent = dummyData[i][dataCategories[j]];
+            // console.log("td = ", td);
+            tr.appendChild(bTag);
+            tr.appendChild(td2);
+        }
+
+        console.log(tr);
+        table.appendChild(tr);
     }
-
-    console.log(tr);
-    table.appendChild(tr);
-  }
 };
 
 //Temporary clearing the space
 
 const clearData = () => {
-  const tableTag = document.querySelector("table");
-  if (document.querySelector("tr")) {
-    tableTag.removeChild(document.querySelector("tr"));
-    tableTag.removeChild(document.querySelector("tr"));
-  }
+    const tableTag = document.querySelector("table");
+    if (document.querySelector("tr")) {
+        tableTag.removeChild(document.querySelector("tr"));
+        tableTag.removeChild(document.querySelector("tr"));
+    }
 }
