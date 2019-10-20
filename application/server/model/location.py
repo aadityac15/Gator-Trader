ae497983019a033e16517c29a07ce5b80f28da1b
SmithManC <chris@drcpx.com>
2019-10-19 18:15:02 -0700

    Update location.py

---
+++ application/server/model/location.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Location(db.Model):
+    location_id = db.Column("location_id", db.INT, primary_key=True, nullable=False, autoincrement=True)
+    description = db.Column("description", db.VARCHAR(length=280), nullable=False, unique=True)
+    thumbnail = db.Column("thumbnail", db.VARCHAR(length=45), nullable=True)
+    created_by = db.Column("created_by", db.INT, db.FOREIGNKEY("user.user_id"), nullable=False)
