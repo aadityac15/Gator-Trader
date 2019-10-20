b3940c705f9b951a2ad50385c659e9d788e192c1
Adam Bea <adamjbea@gmail.com>
2019-10-20 00:09:42 -0700

    Adding GET and POST implementation
    Added and filled out GET and POST method responses for the listing route. to-do:
    -figure out how to actually serialize the rows SELECTED from the mysqlalchemy tables. they do not jsonify nicely
    -figure out put, should just be a massive if else tree?

--- application/server/routes/listings.py
+++ application/server/routes/listings.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify, Response
+from model import listing, location, message, user
+from app import db
 import datetime
 
 listings_blueprint = Blueprint('listings',
@@ -11,23 +13,52 @@ listings_blueprint = Blueprint('listings',
 def get_listings():
     query = request.args.get('query')
     category = request.args.get('category')
+    if query and category:
+        result = listing.query.filter(listing.title == query or listing.type == category)
+        if result:
+            return result
 
-    print(query, category)
+    elif query:
+        result = listing.query.filter(listing.title == query)
+        if result:
+            return result
+    elif category:
+        result = listing.query.filter(listing.type == category)
+        if result:
+            return result
+    else:
+        result = listing.query.all()
+        if result:
+            return result
 
-    # TODO add jsonify functionality
-    return "This is a return"
+    # TODO figure out how to properly return
+
 
 @listings_blueprint.route('/listings', metods=['POST'])
 def post_listing():
-    item_id = request.form.get('item_id')
+    listing_id = request.form.get('listing_id')
     title = request.form.get('title')
-    price = request.form.get('price')
-    quantity = request.form.get('quantity')
     description = request.form.get('description')
-    created_by = datetime.datetime.now()
-    type = request.form.get('time')
-    status = request.form.get('status')
-    approved_by = request.form.get('approved_by')
-    image = request.form.get('image')
+    type = request.form.get('type')
+    price = request.form.get('price')
+    thumbnail = request.form.get('thumbnail')
+    created_on = datetime.datetime.now()
+    last_edited_on = request.form.get('last_edited_on')
+    created_by = request.form.get('created_by')
+
+    new_listing = listing(listing_id=listing_id,
+                          title=title,
+                          description=description,
+                          type=type,
+                          price=price,
+                          thumbnail=thumbnail,
+                          created_on=created_on,
+                          last_edited_on=last_edited_on,
+                          created_by=created_by)
+
+    db.session.add(new_listing)
+    db.session.commit()
+
+    return
 
     # TODO do shit with this shit
