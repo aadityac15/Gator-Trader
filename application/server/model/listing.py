cf905e2c2e7326faeb00543b8493c1baf693a975
Sean Darryanto <seandarryanto@gmail.com>
2019-10-20 02:47:42 -0700

    model changes slight

--- application/server/model/listing.py
+++ application/server/model/listing.py
@@ -2,15 +2,16 @@ from model import db
 
 
 class Listing(db.Model):
-    listing_id = db.Column("listing_id", db.INT, primary_key=True, nullable=False, autoincrement=True)
-    title = db.Column("title", db.VARCHAR(length=45), nullable=False)
+    category = db.Column("category", db.VARCHAR(length=45), nullable=True)
+    created_by = db.Column("created_by", db.INT, db.ForeignKey("user.user_id"), nullable=False)
+    created_on = db.Column("created_on", db.INT, db.ForeignKey("user.user_id"), nullable=False)
     description = db.Column("description", db.VARCHAR(length=280), nullable=True)
-    type = db.Column("type", db.VARCHAR(length=45), nullable=True)
+    last_edited_on = db.Column("last_edited_on", db.DATETIME, nullable=False)
+    listing_id = db.Column("listing_id", db.INT, primary_key=True, nullable=False, autoincrement=True)
     price = db.Column("price", db.DECIMAL(asdecimal=False), default=0.0, nullable=False)
     thumbnail = db.Column("thumbnail", db.VARCHAR(length=128), nullable=True)
-    created_on = db.Column("created_on", db.INT, db.ForeignKey("user.user_id"), nullable=False)
-    last_edited_on = db.Column("last_edited_on", db.DATETIME, nullable=False)
-    created_by = db.Column("created_by", db.INT, db.ForeignKey("user.user_id"), nullable=False)
+    title = db.Column("title", db.VARCHAR(length=45), nullable=False)
+    type = db.Column("type", db.VARCHAR(length=45), nullable=True)
 
     @property
     def serialize(self):
