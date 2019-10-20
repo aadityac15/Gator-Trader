d68d0ecb5ea5068349174c3670538133bba97205
SmithManC <chris@drcpx.com>
2019-10-19 18:19:29 -0700

    Update message.py + location.py fixes

--- application/server/model/location.py
+++ application/server/model/location.py
@@ -4,5 +4,5 @@ from app import db
 class Location(db.Model):
     location_id = db.Column("location_id", db.INT, primary_key=True, nullable=False, autoincrement=True)
     description = db.Column("description", db.VARCHAR(length=280), nullable=False, unique=True)
-    thumbnail = db.Column("thumbnail", db.VARCHAR(length=45), nullable=True)
+    thumbnail = db.Column("thumbnail", db.VARCHAR(length=128), nullable=True)
     created_by = db.Column("created_by", db.INT, db.FOREIGNKEY("user.user_id"), nullable=False)
