eefbb89cf8325019d02fbd506d3207a575d977d2
aadityac15 <achokshi@mail.sfsu.edu>
2019-11-10 22:35:55 -0800

    Changes/prettified the listing page / DB working

--- application/server/client/scripts/domCreation.js
+++ application/server/client/scripts/domCreation.js
@@ -6,10 +6,3 @@ const selectDomElement = (id) => {
 	return document.getElementById(id);
 }
 
-const clearData = () => {
-  const tableTag = document.querySelector("table");
-  if (document.querySelector("tr")) {
-    tableTag.removeChild(document.querySelector("tr"));
-    tableTag.removeChild(document.querySelector("tr"));
-  }
-};
--- application/server/client/scripts/search.js
+++ application/server/client/scripts/search.js
@@ -3,82 +3,166 @@ function selectDropDown(sel) {
 }
 
 const fetchData = async () => {
+  const ulResult = document.getElementById("resultList");
+  clearData(); //Temporary
+
   // Domcreator.js
-  // clearData(); //Temporary
   const selectDropDownElement = document.getElementById("selectDropDown");
   const category =
     selectDropDownElement.options[selectDropDownElement.selectedIndex].value;
   const query = document.getElementById("queryTag").value;
-
-  const LISTINGS_URL = `http://localhost:3000/listings?category=${category}&query=${query}`;
+  ulResult.classList.add("list-group");
+  const LISTINGS_URL = `listings?category=${category}&query=${query}`;
   let dummyLength = 0;
   let dummyData = undefined;
-  const table = createDomElement("table");
-  table.style["border"] = "1px black solid";
 
-  await fetch("/listings", {
+  // const table = document.getElementById("resultTable");
+  // table.style["border"] = "1px black solid";
+
+  await fetch(LISTINGS_URL, {
     method: "GET"
   })
     .then(response => {
-      console.log(response);
-      response.json();
+      return response.text();
     })
     .then(data => {
-      console.log(data);
-      dummyData = data;
+      let dataJson = JSON.parse(data);
+      // console.log(data);
+      console.log("Type of data", typeof dataJson);
+      dummyData = dataJson["listings"];
       //As the data would be an object
       dummyLength = dummyData.length;
-      for (let i = 0; i < dummyLength; i++) {
-        // Creation of Tags.
-        let tr = createDomElement("tr");
-        let td1 = createDomElement("td"); // Td tag for the tables.
-        let td2 = createDomElement("td"); // td tag for the tables.
-        let div = createDomElement("div");
+      dummyData.map(indList => {
+        const titleBTag = createDomElement("b");
+        const h2Tag = createDomElement("h2");
+        const h4Tag = createDomElement("h4");
+        const imgDivTag = createDomElement("div");
+        const imgTag = createDomElement("img");
+        const typeTag = createDomElement("p")
+        const descriptionTag = createDomElement("p");
+        const listingTag = createDomElement("p");
+        const priceTag = createDomElement("p");
+        const titleTag = createDomElement("p");
 
-        // The object is a json object.
-        let dataCategories = Object.keys(dummyData[i]);
+        const divTag = createDomElement("div");
 
-        // Parsing through all the categories and making the corresponding rows.
-        for (let j = 0; j < dataCategories.length; j++) {
-          // Styling the table row.
-          tr.style["border-bottom"] = "1px solid black";
-          let bTag = createDomElement("b"); // Bold Tag
-          let pTag = createDomElement("p"); // p tag for the text in the table.
-          if (
-            ["sold_by", "title", "image", "category", "listing_id"].includes(
-              dataCategories[j]
-            )
-          ) {
-            // Setting the value.
-            if (dataCategories[j] === "image") {
-              let imgTag = createDomElement("img");
-              imgTag.src = dummyData[i][dataCategories[j]];
-              td1.appendChild(imgTag);
-            } 
-            else {
+        // divTag.classList.add("ul-changes");
+        console.log("the indListing is", indList);
+        let liTag = createDomElement("li");
+        styleLi(liTag);
 
-              // Title to be bold.
-              if (dataCategories[j] === "title") {
-              pTag.textContent = dummyData[i][dataCategories[j]];
-               bTag.appendChild(pTag);
-               div.appendChild(bTag); 
-              }
-              else {
-                pTag.textContent = dummyData[i][dataCategories[j]];
-                div.appendChild(pTag);
-              }
-            }
-            div.classList.add("center-align", "flex-start");
-            td2.appendChild(div);
-
-            // Append child is used to inject the tag in the dom.
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            table.appendChild(tr);
-          }
+        // Image / Thumbnails.
+        if (indList["thumbnail"] !== null) {
+          imgTag.src = indList["thumbnail"];
+        } else {
+          imgTag.src = "https://via.placeholder.com/150";
+          // divTag.appendChild(imgTag);
         }
-      }
+        imgDivTag.appendChild(imgTag);
+        styleImgDivTag(imgDivTag); // Styles the img div tag.
+        liTag.appendChild(imgDivTag);
+
+        // Commented code at the bottom goes here.
+        
+        // Title Tag and  styling
+        titleTag.innerText = indList["title"];
+        h2Tag.appendChild(titleTag);
+        titleBTag.appendChild(h2Tag);
+        
+        // Description and other tags.
+        descriptionTag.innerText = indList["description"];
+        listingTag.innerText = indList["listing_id"];
+        listingTag.hidden = true;
+        priceTag.innerText = indList["price"] + "$";
+        typeTag.innerText = indList["type"];
+        h4Tag.appendChild(typeTag);
+        // Style tags to remvove padding
+        styleTags([listingTag, descriptionTag, titleTag, priceTag]);
+        divTag.appendChild(titleBTag);
+        divTag.appendChild(listingTag);
+        divTag.appendChild(descriptionTag);
+        // divTag.appendChild(h4Tag);
+        divTag.appendChild(priceTag);
+        
+        styleDiv(divTag);
+        liTag.appendChild(divTag);
+        liTag.classList.add("list-group-item");
+        ulResult.appendChild(liTag);
+      });
     });
 };
 
+const styleTags = element => {
+  element.map(elem => {
+    elem.classList.add("remove-margin");
+  });
+};
 
+const clearData = () => {
+  // const resultUl = document.getElementById("resultList");
+  // element.parentNode.removeChild(element);
+  // const tableTag = document.getElementById("resultList");
+  // if (document.querySelector("li")) {
+  //   element.removeChild(document.querySelector("li"));
+
+  //  while(resultUl.firstChild ){
+  //   resultUl.removeChild( resultUl.firstChild );
+  // }
+
+  document.getElementById("resultList").innerHTML = " ";
+};
+
+const styleLi = liTag => {
+  liTag.style["display"] = "flex";
+  liTag.style["flex-direction"] = "row";
+  liTag.style["justify-content"] = "space-around";
+};
+
+const styleDiv = divTag => {
+  divTag.style["display"] = "flex";
+  divTag.style["flex-direction"] = "column";
+  divTag.style["justify-content"] = "flex-start";
+  divTag.style["width"] = "100%";
+};
+
+const styleImgDivTag = imgDivTag => {
+  imgDivTag.style["width"] = "20%";
+};
+
+// The object is a json object.
+// let dataCategories = Object.keys(dummyData[i]);
+
+// Parsing through all the categories and making the corresponding rows.
+// for (let j = 0; j < dataCategories.length; j++) {
+//   // Styling the table row.
+//   tr.style["border-bottom"] = "1px solid black";
+//   let bTag = createDomElement("b"); // Bold Tag
+//   let pTag = createDomElement("p"); // p tag for the text in the table.
+//   if (
+//     ["sold_by", "title", "image", "category", "listing_id"].includes(
+//       dataCategories[j]
+//     )
+//   ) {
+//     // Setting the value.
+//     if (dataCategories[j] === "image") {
+//       let imgTag = createDomElement("img");
+//       imgTag.src = dummyData[i][dataCategories[j]];
+//       td1.appendChild(imgTag);
+//     }
+//     else {
+
+//       // Title to be bold.
+//       if (dataCategories[j] === "title") {
+//       pTag.textContent = dummyData[i][dataCategories[j]];
+//        bTag.appendChild(pTag);
+//        div.appendChild(bTag);
+//       }
+//       else {
+//         pTag.textContent = dummyData[i][dataCategories[j]];
+//         div.appendChild(pTag);
+//       }
+//     }
+//     div.classList.add("center-align", "flex-start");
+//     td2.appendChild(div);
+
+//     // Append child is used to inject the tag in the dom.
