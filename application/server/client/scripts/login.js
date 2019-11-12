1ae3e4757d809f488b1e11d69e33c9ace4642ac0
Momo Sun <mosun1113@gmail.com>
2019-11-12 01:41:47 -0800

    update-login-signup-landing_page

---
+++ application/server/client/scripts/login.js
@@ -0,0 +1,7 @@
+document.getElementById(".message a").onclick = function() { 
+  
+    document.getElementById("login-form").style.display = "none"; 
+    document.getElementById("reset-form").style.display = "inline-box"; 
+
+} 
+
