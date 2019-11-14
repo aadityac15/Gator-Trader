35f9d329455a30a49f21fc7ac49886631e836418
patkhai <patrick.phyo@gmail.com>
2019-11-14 01:16:30 -0800

    fix the search result css, added momo login,signup pages

--- application/server/client/scripts/login.js
+++ application/server/client/scripts/login.js
@@ -1,9 +1,39 @@
-   var login_form = document.querySelector(".login-form");
-    var reset_password = document.querySelector("#reset-password");
-    var reset_form = document.querySelector(".reset-form");
-    var admin_message = document.querySelector("#admin-message");
-    var admin_login_form = document.querySelector(".admin-login-form");
+document.addEventListener("DOMContentLoaded", function() {
+    let login_form = document.querySelector(".login-form");
+    let reset_password = document.querySelector("#reset-password");
+    let reset_form = document.querySelector(".reset-form");
+    let admin_message = document.querySelector("#admin-message");
+    let admin_login_form = document.querySelector(".admin-login-form");
+    let admin_reset_password = document.querySelector("#admin-reset-password");
+    let submit_reset_password = document.querySelector(".submit-reset-password");
+    let email_reset_form = document.querySelector(".email-reset-form");
 
-    reset_password.addEventListener("click",function(){
-        login_form.style.display = none;
+
+    reset_password.addEventListener("click", function () {
+        login_form.style.display = "none";
+        reset_form.style.display = "block";
+
     })
+
+    admin_message.addEventListener("click", function () {
+        login_form.style.display = "none";
+        admin_login_form.style.display = "block";
+
+    })
+
+    admin_reset_password.addEventListener("click", function () {
+        reset_form.style.display = "block";
+        admin_login_form.style.display = "none";
+    })
+
+    /**
+     * hard code for reset password
+     */
+    submit_reset_password .addEventListener("click", function () {
+        event.preventDefault()
+        reset_form.style.display = "none";
+        email_reset_form.style.display = "block";
+    })
+
+
+});
