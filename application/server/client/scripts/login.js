a08b9070bd653a25ce5f6e70b67058b20715ed9d
Momo Sun <mosun1113@gmail.com>
2019-11-13 18:08:36 -0800

    update

--- application/server/client/scripts/login.js
+++ application/server/client/scripts/login.js
@@ -1,7 +1,15 @@
-document.getElementById(".message a").onclick = function() { 
-  
-    document.getElementById("login-form").style.display = "none"; 
-    document.getElementById("reset-form").style.display = "inline-box"; 
 
-} 
+    var login_form = document.querySelector(".login-form");
+    var reset_password = document.querySelector("#reset-password");
+    var reset_form = document.querySelector(".reset-form");
+    var admin_message = document.querySelector("#admin-message");
+    var admin_login_form = document.querySelector(".admin-login-form");
+
+    reset_password.addEventListener("click",function(){
+        login_form.style.display = none;
+    })
+
+
+
+
 
