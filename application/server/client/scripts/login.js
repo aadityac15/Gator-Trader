d73b8a600032070db3c2a50d1fbfd837d7a45e9c
Momo Sun <mosun1113@gmail.com>
2019-11-14 01:04:23 -0800

    update login page

--- application/server/client/scripts/login.js
+++ application/server/client/scripts/login.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let admin_message = document.querySelector("#admin-message");
     let admin_login_form = document.querySelector(".admin-login-form");
     let admin_reset_password = document.querySelector("#admin-reset-password");
+    let submit_reset_password = document.querySelector(".submit-reset-password");
+    let email_reset_form = document.querySelector(".email-reset-form");
+  
 
     reset_password.addEventListener("click", function () {
         login_form.style.display = "none";
@@ -22,4 +25,15 @@ document.addEventListener("DOMContentLoaded", function() {
         reset_form.style.display = "block";
         admin_login_form.style.display = "none";
     })
+
+    /**
+     * hard code for reset password
+     */
+    submit_reset_password .addEventListener("click", function () {
+        event.preventDefault()
+        reset_form.style.display = "none";
+        email_reset_form .style.display = "block";
+    })
+
+    
 });
