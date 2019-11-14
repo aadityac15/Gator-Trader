f22b12701c65a9316c8d35ce7f462431e67d90fe
Kam Khai <patrick.phyo@gmail.com>
2019-11-13 21:30:41 -0800

    login changes

---
+++ application/server/client/scripts/login.js
@@ -0,0 +1,9 @@
+   var login_form = document.querySelector(".login-form");
+    var reset_password = document.querySelector("#reset-password");
+    var reset_form = document.querySelector(".reset-form");
+    var admin_message = document.querySelector("#admin-message");
+    var admin_login_form = document.querySelector(".admin-login-form");
+
+    reset_password.addEventListener("click",function(){
+        login_form.style.display = none;
+    })
