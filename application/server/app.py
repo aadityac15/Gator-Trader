b4cb33c460b86643e425c4b63e0670f647f0d96c
patkhai <patrick.phyo@gmail.com>
2019-11-19 22:46:05 -0800

    fix the sign up js for restricting users if they dont put password

--- application/server/app.py
+++ application/server/app.py
@@ -13,7 +13,7 @@ from routes.static_server import static_blueprint
 app = Flask(__name__, static_url_path='', static_folder='client')
 
 # TODO: CHANGE PASSWORD FOR YOUR LOCAL SETUP
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Greeks98@127.0.0.1:3306/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://patkhai:Greeks98@127.0.0.1:3306/test'
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_PORT'] = '3306'
 app.config['MYSQL_USER'] = 'patkhai'
