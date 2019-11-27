22c21cc9ec3c53980f6352a9b8609639c71cac5f
Sean Darryanto <seandarryanto@gmail.com>
2019-11-26 20:36:17 -0800

    GET messages endpoint

--- application/server/app.py
+++ application/server/app.py
@@ -1,32 +1,27 @@
 from flask import Flask, render_template, send_from_directory
 
+# from routes.users import users_blueprint
 from model import db
 from routes.about_me_static import about_blueprint
 from routes.listing import listing_blueprint
 from routes.listings import listings_blueprint
+from routes.message import message_blueprint
 from routes.mylistings import mylistings_blueprint
-from routes.user import user_blueprint
 from routes.static_server import static_blueprint
-# from routes.users import users_blueprint
-# from routes.message import message_blueprint
+from routes.user import user_blueprint
 
 app = Flask(__name__, static_url_path='', static_folder='client')
 
 # TODO: CHANGE PASSWORD FOR YOUR LOCAL SETUP
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Greeks98@127.0.0.1:3306/test'
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_PORT'] = '3306'
-app.config['MYSQL_USER'] = 'patkhai'
-# TODO: CHANGE PASSWORD FOR YOUR LOCAL SETUP
-app.config['MYSQL_PASSWORD'] = 'Greeks98'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Rmssd1530@127.0.0.1:3306/648_database'
 
 db.init_app(app)
 
-# app.register_blueprint(message_blueprint)
 # app.register_blueprint(users_blueprint)
 app.register_blueprint(about_blueprint)
 app.register_blueprint(listings_blueprint)
 app.register_blueprint(listing_blueprint)
+app.register_blueprint(message_blueprint)
 app.register_blueprint(mylistings_blueprint)
 app.register_blueprint(user_blueprint)
 
--- application/server/routes/message.py
+++ application/server/routes/message.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, send_from_directory, render_template
+from flask import Blueprint, request, jsonify
+
+from model.message import Message
+
 
 message_blueprint = Blueprint('message_server',
                               __name__,
@@ -6,7 +9,17 @@ message_blueprint = Blueprint('message_server',
                               template_folder='../client/public/message')
 
 
-@message_blueprint.route('/message/<path:name>', methods=["GET", "POST"])
-def render_users(name):
-    return render_template('/{}.html'.format(name))
+@message_blueprint.route('/my_messages', methods=['GET'])
+def get_my_messages():
+    """
+    Gets all messages sent to a user
 
+    :param user_id
+    :return:
+    """
+    user_id = request.args.get('user_id')
+
+    messages = Message.query.filter_by(sent_to=user_id)
+    return jsonify({
+        'messages': [message.serialize for message in messages]
+    })
