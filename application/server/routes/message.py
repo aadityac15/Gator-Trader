8878caa49539fbd3ebbd5bc694e6a1a3a5cb5650
Sean Darryanto <seandarryanto@gmail.com>
2019-11-26 23:15:05 -0800

    POST and GET message

--- application/server/routes/message.py
+++ application/server/routes/message.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, jsonify
+import datetime
+import distutils
 
 from model.message import Message
+from model import db
 
 
 message_blueprint = Blueprint('message_server',
@@ -23,3 +26,58 @@ def get_my_messages():
     return jsonify({
         'messages': [message.serialize for message in messages]
     })
+
+
+@message_blueprint.route('/send_message', methods=['POST'])
+def send_message():
+    """
+    Sends message to user
+
+    :param sent_by
+    :param sent_to
+    :param message_body
+    :param from_admin
+    :return:
+    """
+    sent_by = request.form.get('sent_by')
+    sent_to = request.form.get('sent_to')
+    message_body = request.form.get('message_body')
+    from_admin = True if request.form.get('from_admin') == 'True' else False
+
+    new_message = Message(
+        sent_by=sent_by,
+        sent_to=sent_to,
+        message_body=message_body,
+        from_admin=from_admin,
+        timestamp=datetime.datetime.now(),
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify({
+        'message_id': new_message.message_id,
+        'timestamp': new_message.timestamp
+    })
+
+
+@message_blueprint.route('/message', methods=['GET'])
+def get_message():
+    """
+    Get message by message_id
+
+    :param message_id
+    :return:
+    """
+    message_id = request.args.get('message_id')
+
+    message = Message.query.get(message_id)
+    if not message:
+        return jsonify({
+            'error': 'No message found'
+        })
+
+    return jsonify({
+        'message': message.serialize
+    })
+
