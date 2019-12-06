154a633770ee993b34ec7a0470ea58d4080cf9c0
Sean Darryanto <seandarryanto@gmail.com>
2019-12-06 00:35:30 -0800

    getListings fetch function
    TODO: Implement createListingTableRow

--- application/server/client/scripts/admin.js
+++ application/server/client/scripts/admin.js
@@ -1,19 +1,51 @@
 const PENDING_LISTINGS_URL = '/pending_listings'
 const APPROVED_LISTINGS_URL = '/approved_listings'
+const ALL_LISTINGS_URL = '/listings'
 
 const getPendingListings = async () => {
-    // TODO: Fetch pending listings and throw in array
+    let pendingListings = getListings(PENDING_LISTINGS_URL);
+    // TODO: Inject into table
 };
 
 
 const getApprovedListings = async () => {
-    // TODO: Fetch approved listings and throw in array
+    let approvedListings = getListings(APPROVED_LISTINGS_URL);
+    // TODO: Inject into table
 };
 
 
 const getAllListings = async () => {
-    // TODO: Fetch all listings and throw in array
+    let allListings = getListings(ALL_LISTINGS_URL);
+    // TODO: Inject into table
 };
+
+const getListings = async (url) => {
+    await fetch(url, {
+        method: "GET",
+        withCredentials: true
+    }).then(response => {
+        if (response === undefined);
+        else {
+            return response.text;
+        }
+    }).then(data => {
+        let listings = [];
+
+        try {
+            let listingsObject = JSON.parse(data);
+            let listings = listingsObject['listings'];
+            if (listings.length == 0) {
+                // TODO: Create table row that says "No listings at the moment."
+            }
+
+            listings.map(listing => createListingTableRow(listing));
+        } catch (e) {
+            // TODO: Create table row that says "No listings at the moment."
+        }
+
+        return listings;
+    });
+}
 
 
 const createListingTableRow = (listing) => {
