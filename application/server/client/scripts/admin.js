5527e92a5267a57df54f1fd2531631343edfba8b
Sean Darryanto <seandarryanto@gmail.com>
2019-12-06 00:11:02 -0800

    admin script skeleton

---
+++ application/server/client/scripts/admin.js
@@ -0,0 +1,21 @@
+const PENDING_LISTINGS_URL = '/pending_listings'
+const APPROVED_LISTINGS_URL = '/approved_listings'
+
+const getPendingListings = async () => {
+    // TODO: Fetch pending listings and throw in array
+};
+
+
+const getApprovedListings = async () => {
+    // TODO: Fetch approved listings and throw in array
+};
+
+
+const getAllListings = async () => {
+    // TODO: Fetch all listings and throw in array
+};
+
+
+const createListingTableRow = (listing) => {
+    // TODO: Creates listing table row to be injected into html
+};
