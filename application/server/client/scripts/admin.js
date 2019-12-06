057ce67771a26d27c77aaf1ea3bfaf798a32f3a2
Sean Darryanto <seandarryanto@gmail.com>
2019-12-06 02:05:15 -0800

    Table row creation and populating done
    TODO: Different buttons for pending, all, approved rows

--- application/server/client/scripts/admin.js
+++ application/server/client/scripts/admin.js
@@ -6,6 +6,9 @@ const getPendingListings = async () => {
     let pendingListings = await getListings(PENDING_LISTINGS_URL);
     console.log(pendingListings);
 
+    const listingTableBody = document.getElementById("pending-listings-table")
+        .getElementsByTagName('tbody')[0];
+    console.log(listingTableBody);
     // TODO: Inject into table
 };
 
@@ -22,7 +25,11 @@ const getAllListings = async () => {
     let allListings = await getListings(ALL_LISTINGS_URL);
     console.log(allListings);
 
-    // TODO: Inject into table
+    const listingTableBody = document.getElementById("all-listings-table")
+        .getElementsByTagName('tbody')[0];
+    console.log(listingTableBody);
+
+    allListings.forEach(listing => listingTableBody.append(listing));
 };
 
 const getListings = async (url) => {
@@ -44,17 +51,50 @@ const getListings = async (url) => {
                 // TODO: Create table row that says "No listings at the moment."
             }
 
-            listings.map(listing => createListingTableRow(listing));
+            listings = listings.map((listing) => createListingTableRow(listing));
+
+            return listings;
         } catch (e) {
             // TODO: Create table row that says "No listings at the moment."
+            console.log(e);
         }
 
-        return listings;
     });
 }
 
 
-const createListingTableRow = (listing) => {
-    // TODO: Creates listing table row to be injected into html
-    return listing;
+let createListingTableRow = (listing) => {
+    let tableRow = createDomElement("tr");
+    let listingImageData = createDomElement("td");
+    let listingTitleData = createDomElement("td");
+    let listingDescriptionData = createDomElement("td");
+    let listingApprovalData = createDomElement("td");
+
+    let listingImage = createDomElement("img");
+    listingImage.src = (listing.thumbnail == null) ? "https://via.placeholder.com/100" : listing.thumbnail;
+    listingImage.alt = listing.title;
+    listingImage.height = 100;
+    listingImage.width = 100;
+    listingImageData.appendChild(listingImage);
+
+    listingTitleData.innerText = listing.title;
+    listingDescriptionData.innerText = listing.description;
+
+    let approveButton = createDomElement("button");
+    approveButton.id = "approveButton";
+    approveButton.innerText = "Approve";
+    let denyButton = createDomElement("button");
+    denyButton.id = "denyButton";
+    denyButton.innerText = "Deny";
+    listingApprovalData.appendChild(approveButton);
+    listingApprovalData.appendChild(denyButton);
+
+    tableRow.appendChild(listingImageData);
+    tableRow.appendChild(listingTitleData);
+    tableRow.appendChild(listingDescriptionData);
+    tableRow.appendChild(listingApprovalData);
+
+    console.log(tableRow);
+
+    return tableRow;
 };
